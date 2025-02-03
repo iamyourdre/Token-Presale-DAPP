7e07af6ce07e6ae4f6a7495a77fc8fb02621c72c
iamyourdre <iamyourdre@gmail.com>
2025-02-03 17:00:29 +0700

    Add react-helmet for improved SEO and set page titles in Index and Presale components

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,16 +1,19 @@
-import React, { useContext } from 'react'
-import Hero from '../components/Hero'
+import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet';
 import { ContractContext } from '../contexts/ContractContext';
 import LoadingPage from '../components/LoadingPage';
+import Hero from '../components/Hero';
 
 const Index = () => {
   const { loading } = useContext(ContractContext);
   return (
     <>
-      {loading ? <LoadingPage/> :
-      <Hero/>}
+      <Helmet>
+        <title>Token Presale - $BGT</title>
+      </Helmet>
+      {loading ? <LoadingPage/> : <Hero/>}
     </>
   )
 }
 
-export default Index
+export default Index;
--- frontend/src/pages/Presale.jsx
+++ frontend/src/pages/Presale.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import TokenInfo from '../components/TokenInfo'
 import Buy from '../components/Buy';
+import { Helmet } from 'react-helmet';
 
 const Presale = () => {
   return (
     <div className='pt-24'>
+      <Helmet>
+        <title>Buy $BGT</title>
+      </Helmet>
       <div className="grid grid-cols-2 gap-6">
         <div className="col-span-2 lg:col-span-1 border rounded-2xl px-7 py-3">
           <Buy />
